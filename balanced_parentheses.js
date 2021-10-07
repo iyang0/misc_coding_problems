1f18a2b7cc076d873d76df15eb309173d5068bea
magus0 <harvestashya@gmail.com>
2021-10-06 17:47:39 -0700

    coding challenge

---
+++ balanced_parentheses.js
@@ -0,0 +1,40 @@
+/**
+ * given a string with parentheses, square brackets, or curly braces
+ * return true or false if it is balanced
+ * 
+ * ############################
+ * 
+ * balancedParentheses("(){}"); 
+ *  -> returns true
+ * balancedParentheses("[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]");
+ *  -> returns true
+ * balancedParentheses("({(()))}}");  
+ *  -> returns false
+*/
+function balancedParentheses(str){
+  //make a stack to put opening braces
+  let stack = [];
+  //make map to pair opening with closing braces
+  let charPairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  }
+
+  for(let char of str){
+    //if character in string is opening brace, put into stack
+    if(char === '(' || char === '{' || char === '[' ){
+      stack.push(char);
+    } else if(char === ')' || char === '}' || char === ']' ){
+      //if closing brace, pop the opening off the stack
+      let lastOpening = stack.pop();
+      //check if popped element matches with the closing
+      if(char != map[lastOpening]) return false;
+      
+    }
+  }
+
+  //if there are any opening braces afterwards, return false otherwise it's balanced
+  if(stack.length !== 0) return false;
+  return true;
+}
