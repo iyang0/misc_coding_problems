42b1baa996115f5ba4632c016b65c76c9326d161
magus0 <harvestashya@gmail.com>
2021-12-23 16:45:45 -0800

    leetcode problem 191

---
+++ 191-number-of-1s.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        binary = bin(n)
+        # turn an integer into list of digits
+        digitList = [digit for digit in str(binary)]
+        # make a counter for number of bits
+        bitCounter = {}
+        for digit in digitList:
+            # loop through list of digits and add to counter
+            bitCounter[digit] = bitCounter.get(digit, 0) + 1
+        
+        # return the number of 1s
+        return bitCounter.get('1',0)
