d4081251a1d5a64cf4a958fa51ac11d979a3a1c9
magus0 <harvestashya@gmail.com>
2022-01-30 23:45:49 -0800

    scrambled palindrome check and only go loop through half of regular palindrome check

--- is_palindrome.js
+++ is_palindrome.js
@@ -1,8 +1,31 @@
 function isPalindrome(string) {
-	const len = string.length;
-	for(let i=0; i < len/2; i++){
-		if(string[i] !== string[len - i - 1]) return false
-	}
+    const len = string.length;
+    for(let i=0; i < len/2; i++){
+        if(string[i] !== string[len - i - 1]) return false
+    }
 
-	return true
+    return true
+}
+
+function scrambledPalindromeCheck(string) {
+    let frequency = {};
+    let isEvenLength = (string.length % 2 === 0);
+
+    for (let letter of string) {
+        frequency[letter] = (frequency[letter] || 0) + 1;
+        // delete from object if there are two of the same letter
+        if (frequency[letter] === 2) {
+            delete frequency[letter]
+        }
+    }
+
+    //if there is nothing in the frequency counter and word is even length
+    //or if it's odd length and there is one thing, return true.
+    if (isEvenLength && Object.keys(frequency).length === 0) {
+        return true;
+    } else if (!isEvenLength && Object.keys(frequency).length === 1) {
+        return true;
+    }
+
+    return false;
 }
