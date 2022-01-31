c148c6c130a1938421354247be2054eef864a278
magus0 <harvestashya@gmail.com>
2022-01-30 23:45:19 -0800

    scrambled palindrome check and only go loop through half of regular palindrome check

--- is_palindrome.js
+++ is_palindrome.js
@@ -1,6 +1,6 @@
 function isPalindrome(string) {
 	const len = string.length;
-	for(let i=0; i < len; i++){
+	for(let i=0; i < len/2; i++){
 		if(string[i] !== string[len - i - 1]) return false
 	}
 
