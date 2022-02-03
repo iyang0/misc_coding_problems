ac9dd952244cba507514b7b3fe015cbf6a88bb88
magus0 <harvestashya@gmail.com>
2022-02-03 15:25:09 -0800

    insertion sort

---
+++ insertion_sort.js
@@ -0,0 +1,26 @@
+function insertionSort(array) {
+	let sortedIdx = 0;
+	
+	while(sortedIdx < array.length){
+		let lowestUnsorted = array[sortedIdx];
+		let lowestUnsortedIdx = sortedIdx;
+		
+		for(let i=sortedIdx; i<array.length; i++){
+			if(array[i] <= lowestUnsorted){
+				lowestUnsorted = array[i];
+				lowestUnsortedIdx = i;
+			}
+		}
+		
+		if(lowestUnsortedIdx !== sortedIdx) swap(array, sortedIdx, lowestUnsortedIdx);
+		
+		sortedIdx++;
+	}
+	return array;
+}
+
+function swap(arr, a, b){
+	const temp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp;
+}
