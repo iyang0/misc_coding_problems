41a75a09f8b1f51a6f796fc4af4509e782bf52a6
magus0 <harvestashya@gmail.com>
2022-04-29 01:16:25 -0700

    adjacent strings password requirement I found interesting

--- adjacent_strings.py
+++ adjacent_strings.py
@@ -1,31 +1,50 @@
 # interesting password limitation I came across once
-from tokenize import String
-from xmlrpc.client import Boolean
 
+QUERTY = [
+    [char for char in "1234567890-= "],
+    [char for char in "qwertyuiop[]\\"],
+    [char for char in "asdfghjkl;'  "],
+    [char for char in "zxcvbnm,./   "]
+]
 
-def is_keyboard_adjacent(str: String) -> Boolean:
+QUERTY_UPPER = [
+    [char for char in "!@#$%^&*()_+ "],
+    [char for char in "QWERTYUIOP{}|"],
+    [char for char in 'ASDFGHJKL:"  '],
+    [char for char in "ZXCVBNM<>?   "]
+]
+
+def is_keyboard_adjacent(str: str) -> bool:
     """
     Given a string, return true if all characters are adjacent to the position
     of the previous character on a qwerty keyboard.
 
     >>> is_keyboard_adjacent("5tgbnmju7")
     True
+    >>> is_keyboard_adjacent("gg")
+    True
     >>> is_keyboard_adjacent("blue")
     False
     """
-    querty = [
-        [char for char in "1234567890-="],
-        [char for char in "qwertyuiop[]"],
-        [char for char in "asdfghjkl;'"],
-        [char for char in "zxcvbnm,./"]
-    ]
-    querty_upper = [
-        [char for char in "!@#$%^&*()_+"],
-        [char for char in "QWERTYUIOP{}"],
-        [char for char in 'ASDFGHJKL:"'],
-        [char for char in "ZXCVBNM<>?"]
-    ]
-    current = str[0]
-    
+    prev_coords = None
     for char in str:
-        pass
+        char_coords = get_char_coords(char)
+        if prev_coords != None:
+            if not is_neighbor_adjacent(prev_coords, char_coords):
+                return False
+        prev_coords = char_coords
+    
+    return True
+
+def is_neighbor_adjacent(coordinate1: tuple, coordinate2: tuple) -> bool:
+    return coordinate1[0] == coordinate2[0] or coordinate1[1] == coordinate2[1]
+
+def get_char_coords(char: str) -> tuple:
+    coordinates_lower = [ (row, col.index(char)) \
+        for row, col in enumerate(QUERTY) \
+        if char in col ]
+    
+    coordinates_upper = [ (row, col.index(char)) \
+        for row, col in enumerate(QUERTY_UPPER) \
+        if char in col ]
+    return coordinates_lower[0] if coordinates_lower else coordinates_upper[0]
